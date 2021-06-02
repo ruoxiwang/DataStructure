3c07fff542777cefa5c71a00176bd62cb6da34d6
ruoxi <wangruoxx@hotmail.com>
2021-06-02 23:27:12 +0800

    cycle linkedlist commit

--- src/com/me/LinkedList.java
+++ src/com/me/LinkedList.java
@@ -34,6 +34,8 @@ public class LinkedList<E> extends AbstractList<E> {
 
     @Override
     public void add(int index, E element) {
+        rangeCheckForAdd(index);
+
         if (index == 0) {
             first = new Node<E>(element, first);
         } else {
@@ -45,6 +47,7 @@ public class LinkedList<E> extends AbstractList<E> {
 
     @Override
     public E remove(int index) {
+        rangeCheck(index);
 
         Node<E> node = first;
         if (index == 0) {
