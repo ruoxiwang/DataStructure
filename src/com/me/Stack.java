37615b4ab3be99164db754d63664af93ba8e97e1
ruoxi <wangruoxx@hotmail.com>
2021-06-06 22:43:11 +0800

    create Stack commit

---
+++ src/com/me/Stack.java
@@ -0,0 +1,29 @@
+package com.me;
+
+public class Stack<E> {
+    private int size;
+
+    public Stack(int size) {
+        this.size = size;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    public void push(E element) {
+
+    }
+
+    public E pop() {
+
+    }
+
+    public E top() {
+
+    }
+}
