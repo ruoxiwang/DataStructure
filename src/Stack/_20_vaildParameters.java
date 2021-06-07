1f40d4e2f6354ef3fd4432fd934a6665eaa2b343
ruoxiwang <wangruoxx@hotmail.com>
2021-06-07 11:08:59 +0800

    Update _20_vaildParameters.java

--- src/Stack/_20_vaildParameters.java
+++ src/Stack/_20_vaildParameters.java
@@ -1,8 +1,32 @@
 package Stack;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class _20_vaildParameters {
+    private static HashMap<Character, Character> map = new HashMap<>();
+
+    static {
+        map.put('(',')');
+        map.put('[',']');
+        map.put('{','}');
+    }
+    public boolean isVaild3(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        int stringLen = s.length();
+        for (int i = 0; i < stringLen; i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                if (c != map.get(stack.pop())) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public boolean isVaild2(String s) {
         Stack<Character> stack = new Stack<>();
 
